035d03c96c73a0f12b6bc93109fbfacf0862dda8
Jzarr-IT-Company <davidwandavid18@gmail.com>
2026-01-31 15:29:48 +0500

    set wordpress

--- src/Pages/WordpressBlogRedirect.jsx
+++ src/Pages/WordpressBlogRedirect.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect , useRef } from 'react';
 
 export default function WordpressBlogRedirect() {
-    const openedRef = useRef(false);
+   // const openedRef = useRef(false);
   useEffect(() => {
-    if (!openedRef.current) {
-        window.open('https://hdpiks.com/blog', '_blank');
-        openedRef.current = true;
-    }
+    // if (!openedRef.current) {
+        
+    // }
+    window.location.replace('https://hdpiks.com/blog');
+        // openedRef.current = true;
   }, []);
 
   return (
